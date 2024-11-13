6786de25d9c13f1fa361dfc8f7b610f4bb4b573c
Melissa Solano Fuentes <msolanofu@ucenfotec.ac.cr>
2024-11-13 11:49:25 -0600

    primer commit

---
+++ src/views/Inicio.vue
@@ -0,0 +1,20 @@
+<script setup>
+import "../assets/inicio.css";
+import { RouterLink, RouterView } from 'vue-router'
+
+
+</script>
+
+<template>
+<header>
+<nav>
+        <RouterLink to="/articulos">Artículos</RouterLink>
+        <RouterLink to="/about">Contacto</RouterLink>
+</nav>
+</header>
+
+<RouterView />
+
+</template>
+
+
